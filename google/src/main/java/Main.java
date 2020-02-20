3a623dd1b51b60d5308acb4ceeea21de374afe3b
Pierre <59824069+ipierre1@users.noreply.github.com>
2020-02-20 21:17:07 +0100

    Main
    Output

--- google/src/main/java/Main.java
+++ google/src/main/java/Main.java
@@ -11,11 +11,13 @@ public class Main {
         File file = new File(args[0]);
 
         try {
+            Hub hub;
             FileReader fileReader = new FileReader(file);
-            PrintWriter writer = new PrintWriter(args[1]);
+            PrintWriter writer = new PrintWriter(args[1], "UTF-8");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = bufferedReader.readLine();
             Integer lineNbr = 1;
+            Integer libraryIndex = 0;
             String[] tabString = line.split(" ");
             ScanPlanning scanPlanning = new ScanPlanning(Integer.parseInt(tabString[0]), Integer.parseInt(tabString[1]), Integer.parseInt(tabString[2]));
             ArrayList<Library> libraries = new ArrayList<>();
@@ -32,7 +34,8 @@ public class Main {
                 if(lineNbr > 2 && lineNbr % 2 != 0) {
                     String[] tabLibrary = line.split("");
                     for (int i = 0; i < tabLibrary.length; i++) {
-                        library = new Library(Integer.parseInt(tabLibrary[0]), Integer.parseInt(tabLibrary[1]), Integer.parseInt(tabLibrary[2]));
+                        library = new Library(Integer.parseInt(tabLibrary[0]), Integer.parseInt(tabLibrary[1]), Integer.parseInt(tabLibrary[2]), libraryIndex);
+                        libraryIndex++;
                     }
                 }
                 if(lineNbr > 2 && lineNbr % 2 == 0) {
@@ -47,6 +50,18 @@ public class Main {
                 line = bufferedReader.readLine();
                 lineNbr++;
             }
+            // Output
+            writer.println(hub.getLibraries().size());
+            for (Library l : hub.getLibraries()) {
+                writer.println(l.getName()+" "+l.getScanBooks().size());
+                for (Book b : l.getScanBooks()) {
+                    writer.print(b.getId()+" ");
+                }
+                writer.print("\n");
+                
+            }
+
+
             writer.close();
             bufferedReader.close();
             fileReader.close();
